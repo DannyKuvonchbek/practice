ccfc264d80e2c2be6a99d984a016e1b9d9985745
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-05-11 22:54:23 +0900

    feat: study iterable object

---
+++ objects.py
@@ -0,0 +1,23 @@
+''' OBJECTS
+    (1) What is object
+    (2) Iterable objects & RANGE
+    (3) DICTIONARY
+    (4) Error handling system 
+'''
+
+import array  # package / module
+import math  # package
+print("====== What is object =====")
+# An object has state and method  properties
+# Everything is object in Python!
+
+print(type('Hello World!'))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# Pardigma > Functional programming & OOP
+# OOP 4 CONCEPTS >  Abstraction | Encapsulation | Inheritence | Polimorphism
+result1 = math.ceil(97.7)  # CALL
+print("result1:", result1)
