d552c72c6adf3163016cfb825bfde6da7d040035
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-05-13 23:40:11 +0900

    feat: Polimorphism concept

--- class.ip.py
+++ class.ip.py
@@ -6,6 +6,7 @@
 
 print("========== INHERITENCE ========")
 # Parent > CHILD
+# Parent only public & protected properties (state + method)
 
 
 class Animal:  # Parent
@@ -46,14 +47,46 @@ class Cat(Animal):  # Child
         print(f"{self.name} says: {self.sound}-{self.sound}")
 
     def play(self):
-        print("play")
+        pass
+
+
+class Fish(Animal):  # Child
+
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def swim(self):
+        print("Yes, I can swim!")
 
 
 dog = Dog("Rex", "wow", True)
 cat = Cat("Tom", "myeow", True)
+fish = Fish("Nemo", "ZzzZ", False)
+
 
 dog.introduce()
 cat.introduce()
 
 print(dog.voice)
 print("status", dog.status)
+
+print("========== POLIMORPHISM ========")
+
+# fish > Fish > Animal > object
+a = isinstance(fish, Fish)
+b = isinstance(fish, Animal)
+c = isinstance(fish, object)
+
+
+result = a and b and c
+print(f"the result: {result}")
+
+# Fish > Animal > object
+data1 = issubclass(Fish, Animal)
+data2 = issubclass(Animal, object)
+print("data:", data1, data2)
