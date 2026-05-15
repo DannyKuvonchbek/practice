8b33123b9a0819ae5ae5ccc289092a395df4e728
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-05-15 23:30:09 +0900

    feat: practice lambda function

--- list.py
+++ list.py
@@ -81,3 +81,27 @@ print("sort reverse:", numbers)
 numbs = [2, 20, 12, 300]
 new_numbs = sorted(numbs)
 print(f"the sorted numbs: {numbs} and new_numbs: {new_numbs}")
+
+
+print("========== Lambda functions ==========")
+# lambda is small anonymous function!
+def calculate(x, y): return x * y
+
+
+result = calculate(3, 5)
+print("result:", result)
+
+people = [
+    ("Robert", 20),
+    ("Steve", 19),
+    ("DANNY", 29),
+    ("Michael", 30),
+    ("Ali", 40),
+]
+people.sort()
+print("people(1)", people)
+
+# sort by age via lambda
+
+people.sort(key=lambda person: person[1])
+print("people(2)", people)
