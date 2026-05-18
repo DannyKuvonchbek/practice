a022813fdd2fe1ca6a17338a89329f7dacc5a301
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-05-18 22:07:33 +0900

    feat: Pip & external packages

--- debugging.py
+++ debugging.py
@@ -3,19 +3,19 @@
    (2) Package Manager & External Packages
    (3) Debugging
 '''
-
+from PIL import Image
 import turtle
 print("========== Python packages & Core Packages ==========")
 ''' Python Packages/Modules: Core , File and  External Packages'''
 # Core Packages > https://docs.python.org/3/library
 
 # Core Packages
-t = turtle.Turtle()
-t.shape("turtle")
-t.speed(2)
-t.circle(150)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.speed(2)
+# t.circle(150)
 
-turtle.done()
+# turtle.done()
 
 print("==========")
 my_file = open("material/message.txt", "r")
@@ -30,3 +30,16 @@ with open("material/message.txt", "r") as your_file:
     your_content = your_file.read()
     print("your_content:", your_content)
 print("DONE")
+
+print("========== Package Manager & External Packages ==========")
+''' Package Manager:
+    Python pip pipenv
+    NodeJs > npm yarn
+    Php > composer 
+    MacOS > brew
+'''
+# External Packages > https://pypi.org/
+with Image.open("material/person.png") as img_obj:
+    resized_img = img_obj.resize((200, 200))
+    resized_img.show()
+    resized_img.save("material/sample.png")
