023afc1da57840aaecaa6b729bf66d8cb24e494b
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-05-18 22:58:56 +0900

    feat: debugging process

--- debugging.py
+++ debugging.py
@@ -43,3 +43,17 @@ with Image.open("material/person.png") as img_obj:
     resized_img = img_obj.resize((200, 200))
     resized_img.show()
     resized_img.save("material/sample.png")
+
+print("========== Debugging ==========")
+
+
+def get_summary(*args):  # Define
+    total_amount = 0
+    for a in args:
+        total_amount += a
+    return total_amount
+
+
+test = 100
+result = get_summary(1, 2, 3, 4, 5)  # Call
+print("result:", result)
